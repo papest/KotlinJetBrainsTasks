0d1a131d1c02bd72382fded925b47b2b10747b64
Epap <peia@inbox.ru>
2023-09-25 14:00:07 +0300

    Override dispatcher

---
+++ src/main/kotlin/Dispatchers.kt
@@ -0,0 +1,31 @@
+import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
+
+fun main() = runBlocking(Dispatchers.Default) {
+    // Modify this code to make sure data loading and storing
+    // happens in Dispatchers.IO, but data printing in Dispatchers.Default
+    // which is already defined for you as a replacement for Dispatchers.Main
+    val mainJob = launch (Dispatchers.IO) {
+        val data = loadData1()
+
+        launch (Dispatchers.Default) { printData(data)}
+        storeData(data)
+    }
+    mainJob.join()
+}
+
+suspend fun printData(data: CoroutineContext) {
+    delay(3)
+    println(currentCoroutineContext())
+    println(data)
+}
+
+suspend fun storeData(data: CoroutineContext) {
+    delay(3)
+    println("Store..")
+    println(currentCoroutineContext())
+}
+
+suspend fun loadData1() : CoroutineContext {
+    return currentCoroutineContext()
+}
