22149292d1cb5ee4a306dd1817217603348b08b0
Epap <peia@inbox.ru>
2023-09-30 08:42:59 +0300

    Sort with anonymous object

---
+++ src/main/kotlin/ObjectExpression.kt
@@ -0,0 +1,19 @@
+class Sorted {
+    data class Person(val name: String, val age: Int)
+
+    val people = listOf(
+        Person("Alice", 25),
+        Person("Bob", 20),
+        Person("Charlie", 30)
+    )
+
+    val sortedByAge = people.sortedWith(object : Comparator<Person> {
+        override fun compare(p1: Person, p2: Person): Int {
+            return p1.age.compareTo(p2.age)
+        }
+    })
+}
+
+fun main() {
+    println(Sorted().sortedByAge)
+}
