643fd346e47ce7dae9dcc1775b4e39b296efad92
Epap <peia@inbox.ru>
2024-03-12 18:09:20 +0300

    Ktor client example

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,6 +31,12 @@ dependencies {
     implementation("org.jetbrains.exposed:exposed-core:0.38.2")
     implementation("org.jetbrains.exposed:exposed-dao:0.38.2")
     implementation("org.jetbrains.exposed:exposed-jdbc:0.38.2")
+    // https://mvnrepository.com/artifact/io.ktor/ktor-client-core
+    implementation("io.ktor:ktor-client-core:1.4.0")
+    // https://mvnrepository.com/artifact/io.ktor/ktor-client-cio
+    implementation("io.ktor:ktor-client-cio:1.4.0")
+    // https://mvnrepository.com/artifact/io.ktor/ktor-client-websockets-jvm
+    implementation("io.ktor:ktor-client-websockets-jvm:1.4.0")
     // https://mvnrepository.com/artifact/com.h2database/h2
     implementation("com.h2database:h2:2.2.220")
 
---
+++ src/main/kotlin/ktor/Echo.kt
@@ -0,0 +1,30 @@
+package ktor
+
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.features.websocket.*
+import io.ktor.http.*
+import io.ktor.http.cio.websocket.*
+import kotlinx.coroutines.*
+import java.util.Scanner
+
+fun main() {
+    val client = HttpClient(CIO) {
+        install(WebSockets) {
+            pingInterval = 20_000
+        }
+    }
+    runBlocking {
+        client.webSocket(method = HttpMethod.Get, host = "127.0.0.1", port = 8080, path = "/echo") {
+            while (true) {
+                val otherMessage = (incoming.receive() as? Frame.Text)?.readText()
+                otherMessage?.let { println(it) }
+
+                Scanner(System.`in`).nextLine()?.let { userMessage ->
+                    send(userMessage)
+                }
+            }
+        }
+    }
+    client.close()
+}
