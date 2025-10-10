e8b7207990e25d61145dbf1fad32e5a17612b3f0
Epap <peia@inbox.ru>
2025-10-10 08:24:19 +0300

    sortedBy

---
+++ src/main/kotlin/Spell.kt
@@ -0,0 +1,7 @@
+data class Spell(val name: String, val power: Int)
+
+fun main() {
+    val input = readln().split(" ")
+    val spells = input.map { Spell(it.split("-")[0], it.split("-")[1].toInt()) }
+    println(spells.sortedBy { it.name }.sortedByDescending { it.power })
+}
