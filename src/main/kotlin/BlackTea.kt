b62016bfba2db108a766b21282015d4478f06e4d
Epap <peia@inbox.ru>
2025-12-20 09:23:40 +0300

    toString override

---
+++ src/main/kotlin/BlackTea.kt
@@ -0,0 +1,15 @@
+open class Tea(val cost: Int, val volume: Int) {
+    override fun toString(): String {
+        return "cost=$cost, volume=$volume"
+    }
+}
+
+class BlackTea(cost: Int, volume: Int) : Tea(cost, volume) {
+    override fun toString(): String {
+        return "BlackTea{${super.toString()}}"
+    }
+}
+
+fun main() {
+    println(BlackTea(5, 6))
+}
